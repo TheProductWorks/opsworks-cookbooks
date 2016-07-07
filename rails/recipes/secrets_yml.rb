c0f9b769795d591bf792ec86b9e8e402eb0bbdbc
Máté Marjai <mate.marjai@gmail.com>
2016-07-07 22:35:27 +0100

    Added secrets.yml settings generation

---
+++ rails/recipes/secrets_yml.rb
@@ -0,0 +1,26 @@
+
+node[:deploy].each do |app_name, deploy_config|
+  # determine root folder of new app deployment
+  app_root = "#{deploy_config[:deploy_to]}/current"
+
+  # use template 'secrets.yml.erb' to generate 'config/secrets.yml'
+  template "#{app_root}/config/secrets.yml" do
+    source "secrets.yml.erb"
+    cookbook "secrets-config"
+
+    # set mode, group and owner of generated file
+    mode "0660"
+    group deploy_config[:group]
+    owner deploy_config[:user]
+
+    # define variable “@secrets” to be used in the ERB template
+    variables(
+      :key_base => deploy_config[:environment_variables]["SECRET_KEY_BASE"] || ""
+    )
+
+    # only generate a file if there is secrets configuration
+    not_if do
+      deploy_config[:environment_variables]["SECRET_KEY_BASE"].blank?
+    end
+  end
+end

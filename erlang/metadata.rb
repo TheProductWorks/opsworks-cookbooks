d67577c275bfe5c57645565d02cffd2908d83f13
Máté Marjai <mate.marjai@gmail.com>
2016-07-08 22:05:55 +0100

    Erlang metadata update.

--- erlang/metadata.rb
+++ erlang/metadata.rb
@@ -6,14 +6,15 @@ description       'Installs erlang, optionally install GUI tools.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '3.0.0'
 
-depends           'apt', '>= 1.7.0'
-depends           'yum', '~> 3.0'
-depends           'yum-epel'
-depends           'yum-erlang_solutions'
+#depends           'apt', '>= 1.7.0'
+#depends           'yum', '~> 3.0'
+#depends           'yum-epel'
+#depends           'yum-erlang_solutions'
 depends           'build-essential'
 
 recipe 'erlang', 'Installs Erlang via native package, source, or Erlang Solutions package'
 recipe 'erlang::package', 'Installs Erlang via native package'
+recipe 'erlang::rebar_install', 'Installs Erlang rebar lib'
 recipe 'erlang::source', 'Installs Erlang via source'
 recipe 'erlang::esl', "Installs Erlang from Erlang Solutions' package repositories"
 

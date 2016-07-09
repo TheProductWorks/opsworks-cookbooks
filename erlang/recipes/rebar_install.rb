95e1cfa1b6ef65237153132252f9b03db7e80e09
Máté Marjai <mate.marjai@gmail.com>
2016-07-09 09:44:45 +0100

    Small updated to the rebar install

--- erlang/recipes/rebar_install.rb
+++ erlang/recipes/rebar_install.rb
@@ -5,15 +5,16 @@
 bash 'install-rebar' do
   Chef::Log.info('Installing Rebar')
   user 'root'
-  cwd '/usr/lib'
+  cwd '/usr/local/src'
 
   code <<-EOH
     [ ! -d rebar ] && git clone git://github.com/rebar/rebar.git
     (cd rebar && ./bootstrap)
-    ln -s /usr/lib/rebar/rebar /usr/bin/rebar
+    ln -s /usr/local/src/rebar/rebar /usr/bin/rebar
   EOH
 
   Chef::Log.info('Rebar successfully installed')
+  action :nothing
   not_if "rebar --version"
 end
 

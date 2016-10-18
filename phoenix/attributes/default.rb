e718fa744f78f3948b4abf1eecff17d1d3b0b33a
James Byrne <jamesdesbyrne@gmail.com>
2016-10-18 13:13:27 +0100

    Added ubuntu / debian specific installs, works better on the AWS instances using apt-get.

--- phoenix/attributes/default.rb
+++ phoenix/attributes/default.rb
@@ -1,30 +1,7 @@
 case node['platform']
 when 'ubuntu'
-  default[:erlang][:packages] = %w{ tar wget make perl gcc m4 ncurses ncurses-devel sed java-1.8.0-openjdk-devel gcc-c++ unixODBC-devel openssl-devel }
-
-  default[:erlang][:source_tar] = "otp_src_18.3.tar.gz"
-  default[:erlang][:source_url] = "http://erlang.org/download/otp_src_18.3.tar.gz"
-  default[:erlang][:source_dir] = "otp_src_18.3"
-  default[:erlang][:root]       = "/usr/local/src/" + default[:erlang][:source_dir]
-  default[:erlang][:bin]        = "/usr/local/bin"
-  default[:erlang][:configure_options] = ""
-
-  default[:elixir][:source_tar] = "v1.3.0.tar.gz"
-  default[:elixir][:source_url] = "https://github.com/elixir-lang/elixir/archive/v1.3.0.tar.gz"
-  default[:elixir][:source_dir] = "elixir-1.3.0"
-  default[:elixir][:root]       = "/usr/local/src/elixir"
-  default[:elixir][:bin]        = node[:elixir][:root] + "/bin"
-
   default[:phoenix][:archive_url] = "https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez"
 
-  default[:nodejs][:packages]             = ['wget']
-  default[:nodejs][:binary_url]           = "https://nodejs.org/dist/v6.2.0/node-v6.2.0-linux-x64.tar.xz"
-  default[:nodejs][:binary_tar]           = "node-v6.2.0-linux-x64.tar.xz"
-  default[:nodejs][:binary_extracted_tar] = "node-v6.2.0-linux-x64"
-  default[:nodejs][:binary_dir]           = "nodejs"
-  default[:nodejs][:root]                 = "/usr/local/src/nodejs"
-  default[:nodejs][:install_root]         = "/usr/local/src/"
-  default[:nodejs][:bin]                  = node[:nodejs][:root] + "/bin"
 when 'redhat', 'centos', 'fedora'
   default[:erlang][:packages] = %w{ tar wget make perl gcc m4 ncurses ncurses-devel sed java-1.8.0-openjdk-devel gcc-c++ unixODBC-devel openssl-devel }
 
--- phoenix/recipes/install_elixir.rb
+++ phoenix/recipes/install_elixir.rb
@@ -2,40 +2,57 @@
 # Cookbook Name:: phoenix
 # Recipe:: install_elixir
 #
+#
 
-elixir_tar_path    = Chef::Config[:file_cache_path] + '/' + node[:elixir][:source_tar]
-elixir_url         = node[:elixir][:source_url]
-elixir_src_path    = Chef::Config[:file_cache_path] + '/' + node[:elixir][:source_dir]
-elixir_root        = node[:elixir][:root]
+case node['platform']
+when 'ubuntu', 'debian'
+  execute "Setup erlang solutions as a package source" do
+    command "wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb"
+    action  :run
+  end
 
-execute "Downloading #{node[:elixir][:source_tar]}" do
-  command "wget --directory-prefix=#{Chef::Config[:file_cache_path]} #{elixir_url}"
-  action  :run
-  not_if { ::File.exists?(elixir_tar_path) }
-end
+  apt_update 'Update source' do
+    action :update
+  end
 
-execute "Extracting Archive #{node[:elixir][:source_tar]}" do
-  cwd     Chef::Config[:file_cache_path]
-  command "tar -zxf #{elixir_tar_path} --directory=#{Chef::Config[:file_cache_path]}"
-  action  :run
-  not_if { ::File.exists?(elixir_src_path) }
-end
+  apt_package 'elixir' do
+    action :install
+  end
+when 'redhat', 'centos', 'fedora'
+  elixir_tar_path    = Chef::Config[:file_cache_path] + '/' + node[:elixir][:source_tar]
+  elixir_url         = node[:elixir][:source_url]
+  elixir_src_path    = Chef::Config[:file_cache_path] + '/' + node[:elixir][:source_dir]
+  elixir_root        = node[:elixir][:root]
 
-execute "Move Built Elixir to Install Location" do
-  command "cp -r #{elixir_src_path} #{elixir_root}"
-  action  :run
-  not_if { ::File.exists?(node[:elixir][:bin]) }
-end
+  execute "Downloading #{node[:elixir][:source_tar]}" do
+    command "wget --directory-prefix=#{Chef::Config[:file_cache_path]} #{elixir_url}"
+    action  :run
+    not_if { ::File.exists?(elixir_tar_path) }
+  end
 
-execute "Make Elixir Source" do
-  cwd     elixir_root
-  command "export PATH=$PATH:#{node[:erlang][:bin]} && make"
-  action  :run
-  not_if { ::File.exists?("#{node[:elixir][:bin]}/iex") }
-end
+  execute "Extracting Archive #{node[:elixir][:source_tar]}" do
+    cwd     Chef::Config[:file_cache_path]
+    command "tar -zxf #{elixir_tar_path} --directory=#{Chef::Config[:file_cache_path]}"
+    action  :run
+    not_if { ::File.exists?(elixir_src_path) }
+  end
 
-execute "Move Built Elixir executables to /usr/local/bin" do
-  command "cp -r #{node[:elixir][:bin]}/* /usr/local/bin/"
-  action  :run
-  not_if { ::File.exists?("/usr/local/bin/iex") }
+  execute "Move Built Elixir to Install Location" do
+    command "cp -r #{elixir_src_path} #{elixir_root}"
+    action  :run
+    not_if { ::File.exists?(node[:elixir][:bin]) }
+  end
+
+  execute "Make Elixir Source" do
+    cwd     elixir_root
+    command "export PATH=$PATH:#{node[:erlang][:bin]} && make"
+    action  :run
+    not_if { ::File.exists?("#{node[:elixir][:bin]}/iex") }
+  end
+
+  execute "Move Built Elixir executables to /usr/local/bin" do
+    command "cp -r #{node[:elixir][:bin]}/* /usr/local/bin/"
+    action  :run
+    not_if { ::File.exists?("/usr/local/bin/iex") }
+  end
 end

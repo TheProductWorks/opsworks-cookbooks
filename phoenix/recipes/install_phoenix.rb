e3da285ccfffa4ce04018be459ada6851f771033
James Byrne <jamesdesbyrne@gmail.com>
2016-10-18 13:15:44 +0100

    Remove the need for the elixir path for phoenix install

--- phoenix/recipes/install_phoenix.rb
+++ phoenix/recipes/install_phoenix.rb
@@ -3,14 +3,12 @@
 # Recipe:: install_phoenix
 #
 
-elixir_bin     = node[:elixir][:bin]
-
 execute "Install Hex for Elixir" do
-  command "export PATH=$PATH:#{elixir_bin} && mix local.hex --force"
+  command "mix local.hex --force"
   action  :run
 end
 
 execute "Install Phoenix Framework" do
-  command "export PATH=$PATH:#{elixir_bin} && mix archive.install #{node[:phoenix][:archive_url]} --force"
+  command "mix archive.install #{node[:phoenix][:archive_url]} --force"
   action  :run
 end

127c179ed73034ce95fb1c76d28453180146df73
Máté Marjai <mate@theproduct.works>
2016-10-21 11:05:22 +0100

    Rework the recipes - move the BASH script into separate executables.

--- deploy/recipes/phoenix.rb
+++ deploy/recipes/phoenix.rb
@@ -17,21 +17,46 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
-  bash 'build the app' do
+  # use template 'prod.secret.exs.erb' to generate 'config/prod.secret.exs'
+  template "#{deploy[:current_path]}/config/prod.secret.exs" do
+    source "prod.secret.exs.erb"
+    cookbook "phoenix-config"
+
+    # set mode, group and owner of generated file
+    mode "0660"
+    group deploy_config[:group]
+    owner deploy_config[:user]
+  end
+
+  execute 'Get dependencies' do
     user 'deploy'
     cwd deploy[:current_path]
-    code <<-EOH
-      mix local.rebar --force
-      mix local.hex --force
-      mix deps.get --only prod
-      MIX_ENV=prod mix compile
-      MIX_ENV=prod mix phoenix.digest
-      MIX_ENV=prod mix release
-    EOH
-    # mix generate
-    #  - TODO make it run as the executable
-    #  start the node:
-    #  deploy[:current_path]/rel/tp_phoenix/bin/tp_phoenix start
-    #  deploy[:current_path]/rel/tp_phoenix/bin/tp_phoenix stop
+    command "mix local.hex --force && mix local.rebar --force && mix deps.get --only prod"
+    action :run
   end
+
+  execute 'Compile' do
+    user 'deploy'
+    cwd deploy[:current_path]
+    command "MIX_ENV=prod mix compile"
+    action :run
+  end
+
+  execute 'Digest' do
+    user 'deploy'
+    cwd deploy[:current_path]
+    command "MIX_ENV=prod mix phoenix.digest"
+    action :run
+  end
+
+  execute 'Make the release' do
+    user 'deploy'
+    cwd deploy[:current_path]
+    command "MIX_ENV=prod mix release"
+    action :run
+  end
+  # mix release
+  #  start the node:
+  #  deploy[:current_path]/rel/tp_phoenix/bin/tp_phoenix start
+  #  deploy[:current_path]/rel/tp_phoenix/bin/tp_phoenix stop
 end

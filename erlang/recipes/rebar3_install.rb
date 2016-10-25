159aedffe2fed67f1d07640713d5ed9de7f7e2d9
Máté Marjai <mate@theproduct.works>
2016-10-25 11:34:10 +0100

    Added rebar3 install recipe

---
+++ erlang/recipes/rebar3_install.rb
@@ -0,0 +1,34 @@
+#
+# Install Rebar using the git repo.
+#
+
+CWD='/usr/local/src'
+REBAR_LOCATION="#{CWD}/rebar3"
+
+git REBAR_LOCATION do
+  Chef::Log.info('Cloning Rebar3 repo')
+  repository 'git@github.com:erlang/rebar3.git'
+  revision 'master'
+  action :sync
+  notifies :run, 'bash[compile_rebar3]', :immediately
+end
+
+bash 'compile_rebar3' do
+  Chef::Log.info('Compiling Rebar3')
+  cwd CWD
+  user 'root'
+
+  code <<-EOH
+    (cd #{REBAR_LOCATION} && ./bootstrap)
+  EOH
+
+  action :nothing
+  not_if "rebar3 --version"
+  notifies :run, 'link[setup_rebar3_executable]', :immediately
+end
+
+link 'setup_rebar3_executable' do
+  target_file "/usr/bin/rebar3"
+  Chef::Log.info('Linking Rebar3')
+  to "#{REBAR_LOCATION}/rebar3"
+end

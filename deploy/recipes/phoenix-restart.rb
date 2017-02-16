b5c2196ef0a5486f161415a019267614b7f737ca
Máté Marjai <mate@theproduct.works>
2017-02-16 14:40:41 +0000

    Elixir restart recipe added.

---
+++ deploy/recipes/phoenix-restart.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: deploy
+# Recipe:: phoenix-restart
+#
+
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'elixir'
+    Chef::Log.debug("Skipping deploy::pheonix application #{application} as it is not an Phoenix / Elixir app")
+    next
+  end
+
+  execute "restart Server" do
+    user deploy[:user]
+    command "/usr/local/tp_api/tp_phoenix/bin/tp_phoenix restart"
+    action :run
+
+    only_if do
+      File.exists?("/usr/local/tp_api/tp_phoenix/bin/tp_phoenix")
+    end
+  end
+
+end
+
+

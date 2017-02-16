2340da6f789226349144215cdfda89f0782d563e
Máté Marjai <mate@theproduct.works>
2017-02-16 14:02:45 +0000

    Added an erlang-stop recipe.

---
+++ deploy/recipes/erlang-stop.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: deploy
+# Recipe:: erlang-stop
+#
+
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'erlang'
+    Chef::Log.debug("Skipping deploy::erlang application #{application} as it is not an Erlang app")
+    next
+  end
+
+  execute "stop Server" do
+    user deploy[:user]
+    command "/usr/local/tp_api/tp_api/bin/tp_api stop"
+    action :run
+
+    only_if do
+      File.exists?(deploy[:current_path])
+    end
+  end
+
+end
+
+

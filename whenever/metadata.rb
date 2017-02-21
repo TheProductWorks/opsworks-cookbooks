6a7d1c08124b2e11e6ce8acae2581b46fbd92f64
Máté Marjai <mate@theproduct.works>
2017-02-21 13:56:32 +0000

    Added task for whenever runner

---
+++ whenever/metadata.rb
@@ -0,0 +1,7 @@
+name             'whenever'
+maintainer       'FreeRunning Technologies'
+maintainer_email 'contact@freerunningtech.com'
+license          'All rights reserved'
+description      'Configures whenever'
+long_description 'WHENEVER'
+version          '0.1.0'

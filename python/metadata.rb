7dd81ecf610ee0ee5b14de13c549ecc062724507
Luke Rickard <luke.x.rickard@gmail.com>
2017-04-04 12:37:46 +0100

    trying alternative version to install python repo

--- python/metadata.rb
+++ python/metadata.rb
@@ -3,5 +3,3 @@ description "Manage python"
 maintainer  "AWS OpsWorks"
 license     "Unknown 0.0"
 version     "3.6.0"
-
-depends 'apt_repository'
--- python/recipes/default.rb
+++ python/recipes/default.rb
@@ -3,12 +3,16 @@
 # Recipe:: default
 #
 
-apt_repository 'ppa:jonathonf' do
-  uri 'https://launchpad.net/~jonathonf/+archive/ubuntu/python-3.6'
-  action :add
+# apt_repository 'ppa:jonathonf' do
+#   uri 'https://launchpad.net/~jonathonf/+archive/ubuntu/python-3.6'
+# end
+
+execute "add-apt-repository-jonathonf-python-3.6" do
+  command "add-apt-repository ppa:jonathonf/python-3.6"
 end
 
 package "python3.6" do
+  action :install
   retries 3
   retry_delay 5
 end

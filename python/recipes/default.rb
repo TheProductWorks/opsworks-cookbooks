05a200693355a82bfb924e28ad90671fb83278ca
Luke Rickard <luke.x.rickard@gmail.com>
2017-04-04 11:32:33 +0100

    adding repository to fetch python from

--- python/recipes/default.rb
+++ python/recipes/default.rb
@@ -3,6 +3,11 @@
 # Recipe:: default
 #
 
+apt_repository 'ppa:jonathonf' do
+  uri 'https://launchpad.net/~jonathonf/+archive/ubuntu/python-3.6'
+  action :add
+end
+
 package "python3.6" do
   retries 3
   retry_delay 5

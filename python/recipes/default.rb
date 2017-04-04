3dddebc97e79b368fd1b82616704b2c8586d0e53
Luke Rickard <luke.x.rickard@gmail.com>
2017-04-04 13:09:51 +0100

    adding apt-get-update to python recipe

--- python/recipes/default.rb
+++ python/recipes/default.rb
@@ -11,6 +11,10 @@ execute "add-apt-repository-jonathonf-python-3.6" do
   command "add-apt-repository ppa:jonathonf/python-3.6"
 end
 
+execute "apt-get-update" do
+  command "apt-get update"
+end
+
 package "python3.6" do
   action :install
   retries 3

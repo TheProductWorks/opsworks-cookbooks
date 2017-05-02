692e3608d004357286cc716825aee2948694cd35
Máté Marjai <mate@theproduct.works>
2017-05-02 11:58:25 +0100

    Updated gunicorn settings when calling the nginx setup recipe.

--- gunicorn/definitions/gunicorn_web_app.rb
+++ gunicorn/definitions/gunicorn_web_app.rb
@@ -3,8 +3,11 @@ define :gunicorn_web_app do
   application = params[:application]
 
   nginx_web_app deploy[:application] do
+    name application
+    application application
     docroot deploy[:absolute_document_root]
     server_name deploy[:domains].first if deploy[:domains]
+    server_aliases deploy[:domains][1, deploy[:domains].size] unless deploy[:domains][1, deploy[:domains].size].empty?
     mounted_at deploy[:mounted_at]
     ssl_certificate_ca deploy[:ssl_certificate_ca]
     cookbook "gunicorn"

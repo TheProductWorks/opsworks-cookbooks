4d1854ebc14ff60b1a9da7b7efddd9edc81a2c42
Máté Marjai <mate@theproduct.works>
2017-05-02 12:04:33 +0100

    Correcting the application name

--- gunicorn/definitions/gunicorn_web_app.rb
+++ gunicorn/definitions/gunicorn_web_app.rb
@@ -2,9 +2,7 @@ define :gunicorn_web_app do
   deploy = params[:deploy]
   application = params[:application]
 
-  nginx_web_app deploy[:application] do
-    name application
-    application application
+  nginx_web_app application do
     docroot deploy[:absolute_document_root]
     server_name deploy[:domains].first if deploy[:domains]
     server_aliases deploy[:domains][1, deploy[:domains].size] unless deploy[:domains][1, deploy[:domains].size].empty?

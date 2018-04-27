1346bbb4df7a8962bb21346228354532a7417c8b
Máté Marjai <mate@theproduct.works>
2018-04-27 15:28:00 +0100

    Use explicit env vars for deploy.

--- deploy/recipes/phoenix-restart.rb
+++ deploy/recipes/phoenix-restart.rb
@@ -10,11 +10,12 @@ node[:deploy].each do |application, deploy|
     Chef::Log.info("Skipping deploy-restart::phoenix application #{application} as it is not an Phoenix / Elixir app")
     next
   end
+  env_vars = deploy[:environment_variables].merge({'MIX_HOME' => '/home/deploy/.mix', 'MIX_ENV' => 'prod', 'HEX_HOME' => '/home/deploy/.hex', 'HOME' => '/home/deploy'})
 
   execute "start Server" do
     user deploy[:user]
     cwd deploy[:current_path]
-    environment 'HOME' => '/home/deploy'
+    environment env_vars
     command "/usr/local/tp_api/tp_phoenix/bin/tp_phoenix start"
     action :run
 
@@ -26,7 +27,7 @@ node[:deploy].each do |application, deploy|
   execute "restart Server" do
     user deploy[:user]
     cwd deploy[:current_path]
-    environment 'HOME' => '/home/deploy'
+    environment env_vars
     command "/usr/local/tp_api/tp_phoenix/bin/tp_phoenix restart"
     action :run
 
